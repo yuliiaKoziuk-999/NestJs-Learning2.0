4540d0fd3c45b6ea36aa84b0ea5dfaca7e7c57c7
yuliiaKoziuk-999 <yuliia.koziuk@fivesysdev.com>
2025-05-27 12:51:16 +0300

    Added test for register method

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -10,97 +10,22 @@ import * as bcrypt from 'bcrypt';
 import { Auth } from 'src/entities/auth.entity';
 import { User } from 'src/users/entities/user.entity';
 import { authenticate } from 'passport';
-import { UnauthorizedException } from '@nestjs/common';
-
-// describe('AuthService', () => {
-//   let authService: AuthService;
-//   let usersService: { findOne: jest.Mock; findbByEmail?: jest.Mock };
-
-//   beforeEach(async () => {
-//     usersService = {
-//       findOne: jest.fn(),
-//       findbByEmail: jest.fn(),
-//     };
-
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [
-//         JwtModule.register({
-//           secret: 'test-secret',
-//           signOptions: { expiresIn: '60s' },
-//         }),
-//       ],
-//       providers: [
-//         AuthService,
-//         {
-//           provide: UsersService,
-//           useValue: {
-//             findOne: jest.fn(),
-//           },
-//         },
-//         {
-//           provide: ConfigService,
-//           useValue: {
-//             get: jest.fn((key: string) => {
-//               if (key === 'JWT_SECRET') return 'test-secret';
-//               if (key === 'JWT_REFRESH_SECRET') return 'test-refresh-secret';
-//               return null;
-//             }),
-//           },
-//         },
-//         {
-//           provide: 'CONFIGURATION(refresh-jwt)',
-//           useValue: {
-//             secret: 'test-refresh-secret',
-//             expiresIn: '7d',
-//           },
-//         },
-//         {
-//           provide: DatabaseService,
-//           useValue: {},
-//         },
-//         {
-//           provide: MyLoggerService,
-//           useValue: {
-//             log: jest.fn(),
-//             error: jest.fn(),
-//           },
-//         },
-//       ],
-//     }).compile();
-
-//     authService = module.get<AuthService>(AuthService);
-//   });
-
-//   it('should be defined', () => {
-//     expect(authService).toBeDefined();
-//   });
-// });
-
-// describe('validateUser', () => {
-//   it('should return user without password if credentials are valid', async () => {
-//     const password = 'testPassword';
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     const testUser = { ...mockUser1, password: hashedPassword };
-
-//     usersService.findByEmail!.mockResolvedValue(testUser);
-
-//     const result = await authService.validateUser(testUser.email, password);
-
-//     expect(result).toBeDefined();
-//     expect(result.email).toBe(testUser.email);
-//     expect(result.password).toBeUndefined();
-//   });
-// });
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import { Role } from 'src/enum/role.enum';
 
 describe('AuthService', () => {
   let service: AuthService;
-  let usersService: { findOne: jest.Mock; findByEmail?: jest.Mock };
+  let usersService: {
+    create: jest.Mock<any, any, any>;
+    findOne: jest.Mock;
+    findByEmail: jest.Mock;
+  };
 
   beforeEach(async () => {
     usersService = {
       findOne: jest.fn(),
       findByEmail: jest.fn(), // додаємо findByEmail тут
+      create: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -188,5 +113,89 @@ describe('AuthService', () => {
         service.validateUser(testUser.email, 'wrongPassword'),
       ).rejects.toThrow(UnauthorizedException);
     });
+  });
+  describe('register', () => {
+    it('should register a new user successfully', async () => {
+      const createAuthDto = {
+        email: `newuser${Date.now()}@gmail.com`,
+        username: 'newuser',
+        password: 'password',
+        roles: [Role.ADMIN],
+      };
+
+      // Користувач не існує
+      usersService.findByEmail.mockResolvedValue(null);
+
+      // Успішне створення користувача
+      const createdUser = {
+        id: 1,
+        email: createAuthDto.email,
+        username: createAuthDto.username,
+        roles: createAuthDto.roles,
+      };
+
+      usersService.create = jest.fn().mockResolvedValue(createdUser);
+
+      const result = await service.register(createAuthDto);
+
+      expect(usersService.findByEmail).toHaveBeenCalledWith(
+        createAuthDto.email,
+      );
+
+      expect(usersService.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          email: createAuthDto.email,
+          username: createAuthDto.username,
+        }),
+      );
+      console.log('Register result:', result);
+
+      expect(result).toEqual({
+        message: 'Реєстрація успішна',
+        user: expect.objectContaining({
+          email: createAuthDto.email,
+          username: createAuthDto.username,
+          // role: 'ADMIN',
+        }),
+      });
+    });
+
+    it('should throw error if user creation fails', async () => {
+      const createAuthDto = {
+        email: 'fail@example.com',
+        username: 'failuser',
+        password: 'password123',
+        roles: [Role.ADMIN],
+      };
+
+      usersService.findByEmail.mockResolvedValue(null);
+
+      usersService.create = jest
+        .fn()
+        .mockRejectedValue(new Error('DB create failed'));
+
+      await expect(service.register(createAuthDto)).rejects.toThrow(
+        'Помилка під час реєстрації: DB create failed',
+      );
+    });
+
+    it('should throw ConflictException if email already exists', async () => {
+      const createAuthDto = {
+        email: 'existing@example.com',
+        username: 'existinguser',
+        password: 'password123',
+        roles: [Role.INTERN],
+      };
+
+      // Користувач вже існує
+      usersService.findByEmail = jest.fn().mockResolvedValue({ id: 1 });
+
+      await expect(service.register(createAuthDto)).rejects.toThrow(
+        'Користувач з такою електронною адресою вже існує',
+      );
+    });
+  });
+  afterEach(() => {
+    jest.clearAllMocks(); // очищає всі мокані виклики
   });
 });
